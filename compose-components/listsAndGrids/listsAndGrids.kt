1c30f3e676ce7869b7f5df81ce6412db3859c76a
Ethan Mofokeng <87164310+mofokengethan@users.noreply.github.com>
2024-02-24 09:20:27 -0500

    Update listsAndGrids.kt

--- compose-components/listsAndGrids/listsAndGrids.kt
+++ compose-components/listsAndGrids/listsAndGrids.kt
@@ -1,4 +1,32 @@
 @Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        // Create references for the composables to constrain
+        val (button, text) = createRefs()
+
+        Button(
+            onClick = { /* Do something */ },
+            // Assign reference "button" to the Button composable
+            // and constrain it to the top of the ConstraintLayout
+            modifier = Modifier.constrainAs(button) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ) {
+            Text("Button")
+        }
+
+        // Assign reference "text" to the Text composable
+        // and constrain it to the bottom of the Button composable
+        Text(
+            "Text",
+            Modifier.constrainAs(text) {
+                top.linkTo(button.bottom, margin = 16.dp)
+            }
+        )
+    }
+}
+
+@Composable
 fun MessageList(messages: List<Message>) {
     Column {
         messages.forEach { message ->
