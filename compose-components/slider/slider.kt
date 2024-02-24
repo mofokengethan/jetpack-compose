e6675b23fc1f0cac5aade559d9113adb70c311d9
Ethan Mofokeng <87164310+mofokengethan@users.noreply.github.com>
2024-02-24 09:15:24 -0500

    Update slider.kt

---
+++ compose-components/slider/slider.kt
@@ -0,0 +1,48 @@
+@Composable
+fun SliderMinimalExample() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it }
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
+
+@Composable
+fun SliderAdvancedExample() {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            colors = SliderDefaults.colors(
+                thumbColor = MaterialTheme.colorScheme.secondary,
+                activeTrackColor = MaterialTheme.colorScheme.secondary,
+                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            steps = 3,
+            valueRange = 0f..50f
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
+
+@Composable
+fun RangeSliderExample() {
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            steps = 5,
+            onValueChange = { range -> sliderPosition = range },
+            valueRange = 0f..100f,
+            onValueChangeFinished = {
+                // launch some business logic update with the state you hold
+                // viewModel.updateSelectedSliderValue(sliderPosition)
+            },
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
