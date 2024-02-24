ef6e000452ac69b08420f50164df03424eb36f4d
Ethan Mofokeng <87164310+mofokengethan@users.noreply.github.com>
2024-02-24 08:39:09 -0500

    Update chips.kt

---
+++ compose-components/chip/chips.kt
@@ -0,0 +1,78 @@
+@Composable
+fun AssistChipExample() {
+    AssistChip(
+        onClick = { Log.d("Assist chip", "hello world") },
+        label = { Text("Assist chip") },
+        leadingIcon = {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = "Localized description",
+                Modifier.size(AssistChipDefaults.IconSize)
+            )
+        }
+    )
+}
+
+@Composable
+fun FilterChipExample() {
+    var selected by remember { mutableStateOf(false) }
+
+    FilterChip(
+        onClick = { selected = !selected },
+        label = {
+            Text("Filter chip")
+        },
+        selected = selected,
+        leadingIcon = if (selected) {
+            {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Done icon",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        } else {
+            null
+        },
+    )
+}
+
+@Composable
+fun InputChipExample(
+    text: String,
+    onDismiss: () -> Unit,
+) {
+    var enabled by remember { mutableStateOf(true) }
+    if (!enabled) return
+
+    InputChip(
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        label = { Text(text) },
+        selected = enabled,
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+    )
+}
+
+@Composable
+fun SuggestionChipExample() {
+    SuggestionChip(
+        onClick = { Log.d("Suggestion chip", "hello world") },
+        label = { Text("Suggestion chip") }
+    )
+}
