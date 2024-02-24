a4ab79b806d820a2c4ce11b569c69f9f8f26d3ac
Ethan Mofokeng <87164310+mofokengethan@users.noreply.github.com>
2024-02-24 08:37:24 -0500

    Update fab.kt

---
+++ compose-components/fab/fab.kt
@@ -0,0 +1,38 @@
+@Composable
+fun Example(onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onClick() },
+    ) {
+        Icon(Icons.Filled.Add, "Floating action button.")
+    }
+}
+
+@Composable
+fun SmallExample(onClick: () -> Unit) {
+    SmallFloatingActionButton(
+        onClick = { onClick() },
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        contentColor = MaterialTheme.colorScheme.secondary
+    ) {
+        Icon(Icons.Filled.Add, "Small floating action button.")
+    }
+}
+
+@Composable
+fun LargeExample(onClick: () -> Unit) {
+    LargeFloatingActionButton(
+        onClick = { onClick() },
+        shape = CircleShape,
+    ) {
+        Icon(Icons.Filled.Add, "Large floating action button")
+    }
+}
+
+@Composable
+fun ExtendedExample(onClick: () -> Unit) {
+    ExtendedFloatingActionButton(
+        onClick = { onClick() },
+        icon = { Icon(Icons.Filled.Edit, "Extended floating action button.") },
+        text = { Text(text = "Extended FAB") },
+    )
+}
